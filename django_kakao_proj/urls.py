9cb34cd4e8ce3962b6997d7a42263e80b245b840
matt700395 <matt7003@korea.ac.kr>
2023-06-01 16:33:56 +0900

    기본 셋팅 완료

--- django_kakao_proj/urls.py
+++ django_kakao_proj/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from kakaomap_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('json/', views.json, name = 'json'),
+    path('', views.index, name = 'index'),
 ]
